60e7eabbdb126bd4e373394ca0ca342daa007587
Thomas Loescher <thomas@loescher.ch>
2020-11-21 10:39:10 +0100

    WIP: fishe

--- Entities/Fish/Fish.gd
+++ Entities/Fish/Fish.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+# node refernce to player
+var player
+
 # random number generator
 var rng = RandomNumberGenerator.new()
 
@@ -7,9 +10,15 @@ var rng = RandomNumberGenerator.new()
 export var speed = 25
 var direction : Vector2
 var last_direction = Vector2(0,1)
+var bounce_countdown = 0
 
 func _ready():
+	player = get_tree().root.get_node("Root/Player")
 	rng.randomize()
 	
 func _process(delta):
-	
+	pass
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
